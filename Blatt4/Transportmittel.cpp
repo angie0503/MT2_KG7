558c09e8c0cb316cef323a1c18fd132ad1b6f146
sophiehunold <sophie.hunold@gmx.de>
2022-05-16 14:35:17 +0200

    Klasse Transportmittel implementiert und Getter hinzugefügt

---
+++ Blatt4/Transportmittel.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "Transportmittel.h"
+using namespace std;
+
+//allgemeiner Konstruktor
+transportmittel(double hoehe, double breite);
+//Methode
+void bewegen(string richtung)
+{
+	x = richtung
+		if (x = norden)
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		if (x = süden)
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		if (x = osten)
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		if (x = westen)
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+}
--- Blatt4/Transportmittel.h
+++ Blatt4/Transportmittel.h
@@ -1,1 +1,21 @@
-#pragma once
+#include <iostream>
+#include <string>
+#include <cstring>
+using namespace std;
+
+class transportmittel
+{
+private:
+	double hoehe;
+	double breite;
+	//allgemeiner Konstruktor
+	transportmittel();
+
+public:
+	//Methode
+	string richtung;
+	void bewegen(string richtung)
+	//getter
+	double get_hoehe();
+	double get_breite();
+}
