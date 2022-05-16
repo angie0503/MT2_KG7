e361fbe07a29f6551613469f787e6094a0772a0c
sophiehunold <sophie.hunold@gmx.de>
2022-05-16 14:37:10 +0200

    Testdatei Transportmittel erstellt und Aufruf der aktuellen Funktionen

---
+++ Blatt4/Transportmitteltest.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <"Transportmittel.h">
+using namespace std;
+
+int main()
+{
+	transportmittel LKW = LKW(3, 2);
+	cout << "Die H\x99he des Transportmittels betr\x84gt" << double get_hoehe << "m" << "und die Breite" << double get_breite << "m" << endl;
+
+	transportmittel Auto = Auto();
+	cout << Auto.bewegen(norden) << endl;
+
+	return 0;
+}
