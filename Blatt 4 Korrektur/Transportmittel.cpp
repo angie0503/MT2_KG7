119f8fe7e9255e58b4173744889e862de08cf8c0
angie0503 <80481395+angie0503@users.noreply.github.com>
2022-05-18 16:06:30 +0200

    Korrektur

---
+++ Blatt 4 Korrektur/Transportmittel.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "Transportmittel.h"
+using namespace std;
+
+
+//Methode
+void bewegen(string richtung)
+{
+	string x = richtung;
+	cin >> x;
+
+		if (x == "Norden")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		else if (x == "Sueden")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		else if (x == "Osten")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+		else if (x == "Westen")
+		{
+			cout << "in Richtung" << x << "bewegen" << endl;
+		}
+}
+
+
+
+
+
+	
+
---
+++ Blatt 4 Korrektur/Transportmitteltest.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+#include "Transportmittel.h"
+using namespace std;
+
+int main()
+{
+	//Test Basisklasse
+	
+	
+
+	
+
+
+
+
+
+
+
+}
