692b4c41a96be354f8797e0b869146b4b127ea72
sophiehunold <sophie.hunold@gmx.de>
2022-05-18 22:21:27 +0200

    Testdatei bearbeitet und öffentliche Klassenmethoden aufgerufen

--- Blatt 4 Korrektur/Transportmittel.cpp
+++ Blatt 4 Korrektur/Transportmittel.cpp
@@ -12,38 +12,70 @@ Transportmittel::Transportmittel(double h, double b)
 }
 
 //Methode
-void bewegen(string Richtung)
+Transportmittel::bewegen(string Richtung) const
 {
 	string x = Richtung;
 	cin >> x;
 
 		if (x == "Norden")
 		{
-			cout << "in Richtung" << x << "bewegen" << endl;
+			cout << "in Richtung" << x << "bewegen\n" << endl;
 		}
 		else if (x == "Sueden")
 		{
-			cout << "in Richtung" << x << "bewegen" << endl;
+			cout << "in Richtung" << x << "bewegen\n" << endl;
 		}
 		else if (x == "Osten")
 		{
-			cout << "in Richtung" << x << "bewegen" << endl;
+			cout << "in Richtung" << x << "bewegen\n" << endl;
 		}
 		else if (x == "Westen")
 		{
-			cout << "in Richtung" << x << "bewegen" << endl;
+			cout << "in Richtung" << x << "bewegen\n" << endl;
 		}
 }
-LandTransportmittel::LandTransportmittel(int r)
+LandTransportmittel::LandTransportmittel(const double h,const double b, int r)
 {
 	radzahl = r;
 }
+LandTransportmittel::fahren(double km)
+{
+	if (km <= 0)
+	{
+		return false;
+	}
+	else
+	{
+		return km;
+	}
+}
+LandTransportmittel::schieben(double km)
+{
+	if (km <= 0)
+	{
+		return false;
+	}
+	else
+	{
+		return km;
+	}
+}
 
-WasserTransportmittel::WasserTransportmittel(double h, double b, double br)
+WasserTransportmittel::WasserTransportmittel(const double h, const double b, double br)
 {
 	hoehe = h;
 	breite = b;
 	bruttoregistertonnen = br;
+}
+WasserTransportmittel::anlegen(string Anlegehafen)
+{
+	cin >> Anlegehafen;
+	cout << "Der Anlegehafen lautet" << Anlegehafen << "\n" << endl;
+}
+WasserTransportmittel::ablegen(string Ablegehafen)
+{
+	cin >> Ablegehafen;
+	cout << "Der Ablegehafen lautet" << Ablegehafen << "\n" << endl;
 }
 
 
--- Blatt 4 Korrektur/Transportmitteltest.cpp
+++ Blatt 4 Korrektur/Transportmitteltest.cpp
@@ -7,10 +7,54 @@ using namespace std;
 int main()
 {
 	//Test Basisklasse
-	Transportmittel Bus;
-	Bus = new Transportmittel (2,3)
-	cout << "Die Hoehe des Transportmittels betraegt" << h << endl;
-	cout << "Die Breite des Transportmittels betraegt" << b << endl;
+
+	const double hoehe;
+	const double breite;
+	string x;
+	double km;
+	string Anlegehafen;
+	string Ablegehafen;
+
+	cout << "Hallo! Bitte geben Sie die Hoehe des Transportmittels ein!\n" << endl;
+	cin >> hoehe;
+	cout << "Geben Sie nun die Breite des Transportmittels ein!\n" << endl;
+	cin >> breite;
+	Transportmittel Bus = Transportmittel(hoehe, breite);
+	cout << "Die Hoehe des Transportmittels betraegt " << Bus.get_hoehe << endl;
+	cout << "Die Breite des Transportmittels betraegt " << Bus.get_breite << endl;
 	
+	//Test der Methode in der Basisklasse
+
+	Transportmittel Bollerwagen = Bollerwagen.bewegen(x);
+	cout << "Bitte geben Sie die Richtung ein, in die Sie sich bewegen wollen!\n" << endl;
+	cin >> x;
+	cout << "in Richtung " << x << " bewegen\n" << endl;
+
+	// Test öffentliche Methoden der abgeleiteten Klassen
+	LandTransportmittel Taxi = Taxi.fahren(km);
+	cout << "Geben Sie die Kilometer an, die gefahren werden sollen!\n" << endl;
+	cin >> km;
+	cout << "Es solllen " << km << "km gefahren werden. \n" << endl;
+
+	LandTransportmittel Schubkarren = Schubkarren.schieben(km);
+	cout << "Geben Sie ein, wie viele km geschoben werden sollen!\n" << endl;
+	cin >> km;
+	cout << "Sie wollen das Transportmittel " << km << "km schieben.\n" << endl;
+
+	WasserTransportmittel Schiff = Schiff.anlegen(Anlegehafen);
+	cout << "Welchen Zielort streben Sie an?\n" << endl;
+	cin >> Anlegehafen;
+	cout << "Der Anlegehafen lautet: " << Anlegehafen << "\n" << endl;
+
+	WasserTransportmittel Kreuzfahrtschiff = Kreuzfahrtschiff.ablegen(Ablegehafen);
+	cout << "Von wo wollen Sie abreisen?\n" << endl;
+	cin >> Ablegehafen;
+	cout << "Ihr Ablegehafen lautet: " << Ablegehafen << "\n" << endl;
+
+	WasserTransportmittel Boot = Boot.bewegen(x);
+	cout << "In welche Richtung möchten Sie sich mit dem Wassertransportmittel bewegen?\n" << endl;
+	cin >> x;
+	cout << "In Richtung" << x << "bewegen\n" << endl;
+
 	return 0;
 }
