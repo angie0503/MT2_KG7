324f01347de9ee2b2695a9accd640ed565f8b493
sophiehunold <sophie.hunold@gmx.de>
2022-05-24 16:05:59 +0200

    Headerdatei mit Operatorfunktionen Addition und Subtraktion hinzugefügt

--- Blatt5/Complex.h
+++ Blatt5/Complex.h
@@ -1,1 +1,32 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <cmath>
+using namespace std;
 
+class Complex
+{
+private:
+
+	double real; //Realteil
+	double imag; // Imaginärteil
+
+public:
+	//allgemeiner Konstruktor
+	Complex(double re, double im);
+
+	//Getter
+	double get_re() { return re; };
+	double get_im() { return im; };
+
+	//Setter
+	double set_re(double re) { real = re; };
+	double set_im(double im) { imag = im; };
+
+	//Operatorfunktion Addition
+	Complex operator+ (Complex b);
+
+	// Operatorfunktion Subtraktion
+	Complex operator- (Complex b);
+};
